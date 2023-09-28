7009b1e6bd541b40d826dc5b50d3b1a286fdc7eb
lucianonhperiera <lucianonhpereira@gmail.com>
2023-09-28 13:42:23 -0300

    se ajustan los tamaños de las fotos automaticament

--- pages/biblioteca_videos/home_videos.js
+++ pages/biblioteca_videos/home_videos.js
@@ -1,46 +1,20 @@
-var imageCarousel = document.getElementById("imageCarousel");
-var images = imageCarousel.querySelectorAll("img");
 
-var currentIndex = 0;
-var intervalId;
+window.onscroll = function() {
+    scrollFunction();
+};
 
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateCarousel();
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
 }
 
-function updateCarousel() {
-    images.forEach(function (image, index) {
-        var isCentered = index === currentIndex;
-        var isLeft = index < currentIndex && index < currentIndex - 2;
-        var isRight = index > currentIndex && index > currentIndex + 2;
-
-        var transform = isCentered ? "translateX(0%)" : isLeft ? "translateX(-40%)" : "translateX(40%)";
-        var className = isCentered ? "centered" : isLeft ? "left-image" : isRight ? "right-image" : "";
-        
-        image.style.transform = transform;
-        image.className = className;
-    });
+// Función para hacer scroll suave hacia arriba al hacer clic en el botón
+function scrollToTop() {
+    document.body.scrollTop = 0; // Para navegadores antiguos
+    document.documentElement.scrollTop = 0; // Para navegadores modernos
 }
-
-function startAutoPlay() {
-    intervalId = setInterval(nextSlide, 5000);
-}
-
-function stopAutoPlay() {
-    clearInterval(intervalId);
-}
-
-images.forEach(function (image, index) {
-    image.addEventListener("mouseenter", function () {
-        currentIndex = index;
-        updateCarousel();
-        stopAutoPlay();
-    });
-
-    image.addEventListener("mouseleave", function () {
-        startAutoPlay();
-    });
-});
-
-startAutoPlay();
