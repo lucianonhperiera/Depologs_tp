f58c1a615e7f62213f6a1e32d4b83cf7b82d0415
ManuCaporaso <manulpda@gmail.com>
2023-09-29 16:43:50 -0300

    post merge con backup luciano

--- home/home.js
+++ home/home.js
@@ -4,10 +4,3 @@ const menuList = document.getElementById('menu-list');
 menuButton.addEventListener('click', () => {
     menuList.style.display = (menuList.style.display === 'block') ? 'none' : 'block';
 });
-
-const menuButton = document.getElementById('menu-button');
-const menuList = document.getElementById('menu-list');
-
-menuButton.addEventListener('click', () => {
-    menuList.style.display = (menuList.style.display === 'block') ? 'none' : 'block';
-});
