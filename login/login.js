e8f0a56d37bb8a01f3cc06917f7e425a87ed2497
ManuCaporaso <manulpda@gmail.com>
2023-10-12 13:11:11 -0300

    Secciones

---
+++ login/login.js
@@ -0,0 +1,4 @@
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    // Aquí puedes agregar el código para verificar las credenciales y realizar el inicio de sesión.
+});
