27640a18e21bf336dbdccc13e433a19def1156b9
Toniirubilar <laboralagus@gmail.com>
2023-10-12 22:01:54 -0300

    galeria

--- pages/deportes/Voley/home_voley.js
+++ pages/deportes/Voley/home_voley.js
@@ -1,10 +1,22 @@
-// JavaScript para mostrar y ocultar el tooltip
-function showTooltip() {
-    var tooltip = document.querySelector('.tooltip');
-    tooltip.style.display = "block";
-}
+const imagenes = document.querySelectorAll(".img-galeria");
+const imagenesLight = document.querySelector(".agregar_imagen");
+const contenedorLight = document.querySelector(".imagen_light");
 
-function hideTooltip() {
-    var tooltip = document.querySelector('.tooltip');
-    tooltip.style.display = "none";
+imagenes.forEach(imagen =>{
+    imagen.addEventListener("click", ()=>{
+        aparecerImagen(imagen.getAttribute("src"))
+    })
+})
+
+contenedorLight.addEventListener("click", (e=>{
+    if(e.target !== imagenesLight){
+        contenedorLight.classList.toggle("show")
+        imagenesLight.classList.toggle("showImage")
+    }
+}))
+
+const aparecerImagen = (imagen) =>{
+    imagenesLight.src = imagen;
+    contenedorLight.classList.toggle("show")
+    imagenesLight.classList.toggle("showImage")
 }
