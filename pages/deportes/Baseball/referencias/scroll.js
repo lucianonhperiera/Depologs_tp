7bdaa7499a287757c6e6dd0d46cbe07a2dc85ff2
Raiden475 <raulcamacho475@gmail.com>
2023-10-17 19:54:21 -0300

    .....

---
+++ pages/deportes/Baseball/referencias/scroll.js
@@ -0,0 +1,16 @@
+// scroll.js
+
+// Función para mostrar u ocultar el botón según la posición de desplazamiento
+function toggleScrollButton() {
+    const scrollButton = document.querySelector(".volver");
+    if (window.scrollY > 200) {
+        scrollButton.style.opacity = 1;
+        scrollButton.style.pointerEvents = "auto";
+    } else {
+        scrollButton.style.opacity = 0;
+        scrollButton.style.pointerEvents = "none";
+    }
+}
+
+// Asigna la función al evento de desplazamiento de la ventana
+window.addEventListener("scroll", toggleScrollButton);
