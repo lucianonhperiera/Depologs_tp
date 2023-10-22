c99bd766033e93c61b121474eb51e8744c3d5b8a
Toniirubilar <laboralagus@gmail.com>
2023-10-22 17:44:45 -0300

    button gotop

--- pages/deportes/Voley/home_voley.js
+++ pages/deportes/Voley/home_voley.js
@@ -20,3 +20,28 @@ const aparecerImagen = (imagen) =>{
     contenedorLight.classList.toggle("show")
     imagenesLight.classList.toggle("showImage")
 }
+
+let lastScrollPosition = window.scrollY; // Variable para rastrear la posición de desplazamiento anterior
+
+window.onscroll = function () {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition < lastScrollPosition) {
+    // El usuario se está desplazando hacia arriba y ha superado 200 píxeles
+    document.querySelector('.gotop_container').classList.add('show2');
+  } else {
+    // El usuario se está desplazando hacia abajo o no ha superado los 200 píxeles
+    document.querySelector('.gotop_container').classList.remove('show2');
+  }
+
+  lastScrollPosition = scrollPosition; // Actualizar la posición de desplazamiento anterior
+};
+
+
+document.querySelector(".gotop_container")
+.addEventListener("click", () =>{
+    window.scrollTo({
+        top: 0,
+        behavior: `smooth`
+    });
+});
