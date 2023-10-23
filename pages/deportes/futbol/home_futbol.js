15e09afa0b55c58165bd281153a0f6a924ecb8fa
ManuCaporaso <manulpda@gmail.com>
2023-10-22 22:44:18 -0300

    AGREGA INFORMACION

--- pages/deportes/futbol/home_futbol.js
+++ pages/deportes/futbol/home_futbol.js
@@ -1,31 +1,45 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.carousel-slide img');
-const totalSlides = slides.length;
+//CARROUSELL DE NOVEDADES
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function changeSlide(direction) {
-    currentIndex += direction;
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-    if (currentIndex < 0) {
-        currentIndex = totalSlides - 1;
-    } else if (currentIndex >= totalSlides) {
-        currentIndex = 0;
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
     }
 
-    updateSlide();
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
-function updateSlide() {
-    slides.forEach((slide, index) => {
-        slide.style.transform = `translateX(${100 * (index - currentIndex)}%)`;
-    });
-}
-
-updateSlide(); // Mostrar la primera imagen
-
-// Autoplay (opcional)
+// Cambiar las imágenes automáticamente cada 5 segundos
 setInterval(() => {
-    changeSlide(1); // Cambiar a la siguiente imagen cada cierto tiempo (por ejemplo, cada 3 segundos)
-}, 3000);
+    plusSlides(1);
+}, 5000);
+
 
 
 
