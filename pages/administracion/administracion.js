551abeb671779fbb83159030f9c6afc7b7221103
lucianonhperiera <lucianonhpereira@gmail.com>
2023-12-01 17:15:04 -0300

    administracion de usuarios

--- pages/administracion/administracion.js
+++ pages/administracion/administracion.js
@@ -1,75 +1,136 @@
-let listaUsuarios = [];
+let listaEmpleados = [];
 
-const objusuario = {
+const objEmpleado = {
     id: '',
     nombre: '',
-    rol: ''
+    puesto: ''
 }
 
 let editando = false;
 
 const formulario = document.querySelector('#formulario');
-
-const nombreImput = document.querySelector('#nombre');
-
-const rolImput = document.querySelector('#rol');
-
-const agregarImput = document.querySelector('#agregar');
+const nombreInput = document.querySelector('#nombre');
+const puestoInput = document.querySelector('#puesto');
+const btnAgregarInput = document.querySelector('#btnAgregar');
 
 formulario.addEventListener('submit', validarFormulario);
 
 function validarFormulario(e) {
     e.preventDefault();
 
-    if (nombreImput.value === '' || rolImput.value === '') {
-        alert("Todos los campos son obligatorios");
-        return
+    if(nombreInput.value === '' || puestoInput.value === '') {
+        alert('Todos los campos se deben llenar');
+        return;
     }
 
-    if(editando){
-        //editandoRol();
-        editando = false
-    }else{
-       objusuario.id = Date.now();
-       objusuario.nombre = nombreImput.value;
-       objusuario.rol = rolImput.value;
+    if(editando) {
+        editarEmpleado();
+        editando = false;
+    } else {
+        objEmpleado.id = Date.now();
+        objEmpleado.nombre = nombreInput.value;
+        objEmpleado.puesto = puestoInput.value;
 
-       agregarUsuario();
+        agregarEmpleado();
     }
-
 }
 
-function agregarUsuario(){
-    listaUsuarios.push({...objusuario})
+function agregarEmpleado() {
 
-    mostrarUsuarios();
+    listaEmpleados.push({...objEmpleado});
+
+    mostrarEmpleados();
+
+    formulario.reset();
+    limpiarObjeto();
 }
 
-function mostrarUsuarios(){
-    const divUsuarios = document.querySelector('.usuarios');
+function limpiarObjeto() {
+    objEmpleado.id = '';
+    objEmpleado.nombre = '';
+    objEmpleado.puesto = '';
+}
 
-    listaUsuarios.forEach( usuario => {
-        const {id, nombre, rol} = usuario;
+function mostrarEmpleados() {
+    limpiarHTML();
+
+    const divEmpleados = document.querySelector('.div-empleados');
+    
+    listaEmpleados.forEach(empleado => {
+        const {id, nombre, puesto} = empleado;
 
         const parrafo = document.createElement('p');
-        parrafo.textContent = ` ${id} - ${nombre} - ${rol} - `;
+        parrafo.textContent = `${id} - ${nombre} - ${puesto} - `;
         parrafo.dataset.id = id;
 
-        const editarBoton = document.createElement('botton');
-        //editarBoton.onclick = () => cargarUsuario(usuario);
-        editarBoton.textContent = 'editar';
-        editarBoton.classList,add('btn', 'btn-editar');
-        parrafo.append('editarboton')
+        const editarBoton = document.createElement('button');
+        editarBoton.onclick = () => cargarEmpleado(empleado);
+        editarBoton.textContent = 'Editar';
+        editarBoton.classList.add('btn', 'btn-editar');
+        parrafo.append(editarBoton);
 
-        const eliminarBoton = document.createElement('botton');
-        //eliminarBoton.onclick = () => eliminarUsuario(id);
-        eliminarBoton.textContent = 'eliminar';
-        eliminarBoton.classList,add('btn', 'btn-eliminar');
-        parrafo.append('eliminarBoton')
+        const eliminarBoton = document.createElement('button');
+        eliminarBoton.onclick = () => eliminarEmpleado(id);
+        eliminarBoton.textContent = 'Eliminar';
+        eliminarBoton.classList.add('btn', 'btn-eliminar');
+        parrafo.append(eliminarBoton);
 
         const hr = document.createElement('hr');
 
-        divUsuarios.appendChild(parrafo);
-        divUsuarios.appendChild(hr);
-    })
+        divEmpleados.appendChild(parrafo);
+        divEmpleados.appendChild(hr);
+    });
+}
+
+function cargarEmpleado(empleado) {
+    const {id, nombre, puesto} = empleado;
+
+    nombreInput.value = nombre;
+    puestoInput.value = puesto;
+
+    objEmpleado.id = id;
+
+    formulario.querySelector('button[type="submit"]').textContent = 'Actualizar';
+    
+    editando = true;
+}
+
+function editarEmpleado() {
+
+    objEmpleado.nombre = nombreInput.value;
+    objEmpleado.puesto = puestoInput.value;
+
+    listaEmpleados.map(empleado => {
+
+        if(empleado.id === objEmpleado.id) {
+            empleado.id = objEmpleado.id;
+            empleado.nombre = objEmpleado.nombre;
+            empleado.puesto = objEmpleado.puesto;
+
+        }
+
+    });
+
+    limpiarHTML();
+    mostrarEmpleados();
+    formulario.reset();
+
+    formulario.querySelector('button[type="submit"]').textContent = 'Agregar';
+    
+    editando = false;
+}
+
+function eliminarEmpleado(id) {
+
+    listaEmpleados = listaEmpleados.filter(empleado => empleado.id !== id);
+
+    limpiarHTML();
+    mostrarEmpleados();
+}
+
+function limpiarHTML() {
+    const divEmpleados = document.querySelector('.div-empleados');
+    while(divEmpleados.firstChild) {
+        divEmpleados.removeChild(divEmpleados.firstChild);
+    }
 }
