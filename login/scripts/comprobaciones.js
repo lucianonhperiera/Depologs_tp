e4955b224a2a86a8679a074c1fff6e2d4e9e81e9
lucianonhperiera <lucianonhpereira@gmail.com>
2023-12-04 14:44:52 -0300

    cambios en login y pruebas de backend

--- login/scripts/comprobaciones.js
+++ login/scripts/comprobaciones.js
@@ -1,6 +1,6 @@
-const mysql = require("mysql");
+import { createConnection } from "mysql";
 
-const connection = mysql.createConnection({
+const connection = createConnection({
   host: "127.0.0.1",
   port: 3306,
   database: "udps",
