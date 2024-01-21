2c460f0ecb71139563d631aad39c2f0b786d23f3
ManuCaporaso <manulpda@gmail.com>
2024-01-21 15:29:35 -0300

    Actualizacion Futbol

--- pages/deportes/futbol/home_futbol.js
+++ pages/deportes/futbol/home_futbol.js
@@ -1,29 +1,40 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.carousel-slide img');
-const totalSlides = slides.length;
+//CARROUSEL DE LIBROS
 
-function changeSlide(direction) {
-    currentIndex += direction;
+const carousel = document.querySelector(".carousel");
 
-    if (currentIndex < 0) {
-        currentIndex = totalSlides - 1;
-    } else if (currentIndex >= totalSlides) {
-        currentIndex = 0;
-    }
+carousel.addEventListener("scroll", () => {
+    // Lógica para mostrar solo un libro a la vez en el carrusel.
+    const scrollLeft = carousel.scrollLeft;
+    const bookWidth = 320; // Ancho de cada libro incluyendo márgenes
+    const bookIndex = Math.floor((scrollLeft + bookWidth / 2) / bookWidth);
 
-    updateSlide();
-}
-
-function updateSlide() {
-    slides.forEach((slide, index) => {
-        slide.style.transform = `translateX(${100 * (index - currentIndex)}%)`;
+    // Lógica para resaltar el libro activo.
+    const books = document.querySelectorAll(".book");
+    books.forEach((book, index) => {
+        book.classList.toggle("active", bookIndex === index);
     });
+});
+
+
+
+
+// Función para mostrar u ocultar el botón según el desplazamiento de la página
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
 }
 
-updateSlide(); // Mostrar la primera imagen
-
-// Autoplay (opcional)
-setInterval(() => {
-    changeSlide(1); // Cambiar a la siguiente imagen cada cierto tiempo (por ejemplo, cada 3 segundos)
-}, 3000);
-
+// Función para hacer scroll suave hacia arriba al hacer clic en el botón
+function scrollToTop() {
+    document.body.scrollTop = 0; // Para navegadores antiguos
+    document.documentElement.scrollTop = 0; // Para navegadores modernos
+}
