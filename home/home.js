2ad5758a243983fcfc7aad141cdcbd7ec3199f1e
ManuCaporaso <manulpda@gmail.com>
2024-01-21 16:01:31 -0300

    Cambios

--- home/home.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById('menu-button');
-const menuList = document.getElementById('menu-list');
-
-menuButton.addEventListener('click', () => {
-    menuList.style.display = (menuList.style.display === 'block') ? 'none' : 'block';
-});
